8657b26a8f2530553dc179bf1bccd352378740cc
Bilal Sarımeşeli <bilalsarimeseli@Bilals-MacBook-Air.local>
2019-07-13 15:14:54 -0700

    Final CSS modifications done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,59 @@
-// Variables
+// Game Variables
 var wins = 0
 var losses = 0
 var attempts = 10;
-var lettersClicked = []
-var computerLetter = ""
+var lettersClicked = [];
+var computerLetter = "";
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
-// functio calulate the computer letter
+
 
 
 // game // get the letters user is clicking and then comapre with the computer lters
 // if ===  wins increae if not attemps === 0 then you lost
 
+
+// The function that generates the random computer letter
 function game() {
 
     var index = Math.floor(Math.random() * alphabet.length)
     computerLetter = alphabet[index]
-    console.log("computer letter: ", computerLetter)
+    console.log("computer letter: ", computerLetter);
 
-    // update the info
+    // updating the entries
 
-    document.getElementById("Wins").innerHTML = "wins: " + wins
-    document.getElementById("Losses").innerHTML = "losses: " + losses
-    document.getElementById("guesses-left").innerHTML = "guesses-left: " + attempts
-    document.getElementById("clicked-letters").innerHTML = "clicked-letters: " + lettersClicked
+    document.getElementById("Wins").innerHTML = "WINS: " + wins
+    document.getElementById("Losses").innerHTML = "LOSSES: " + losses
+    document.getElementById("guesses-left").innerHTML = "REMAINING GUESSES " + attempts
+    document.getElementById("clicked-letters").innerHTML = "CLICKED LETTERS " + lettersClicked
 }
 
-document.onkeydown = function (event) {
-    console.log(event)
-    console.log(event.key)
-    var letterInput = event.key.toLowerCase()
-    document.getElementById("info").innerHTML = ""
+// CLICKING FUNCTION OF THE USER
 
-    ///// validate that the input is an alphabet letter
+document.onkeydown = function (event) {
+    console.log(event);
+    console.log(event.key);
+    var letterInput = event.key.toLowerCase();
+    document.getElementById("info").innerHTML = "";
+
+    ///// Validate that the input is an alphabet letter;
+
     if (alphabet.indexOf(letterInput) === -1 || lettersClicked.indexOf(letterInput) !== -1) {
-        document.getElementById("info").innerHTML = "please just a letter and not repetition"
+        document.getElementById("info").innerHTML = "Please enter just a letter without repetition!"
     }
     else {
 
-        // validate users is not clicking the same letter twice
-        attempts--
-        document.getElementById("guesses-left").innerHTML = "guesses-left: " + attempts
-        lettersClicked.push(letterInput)
-        document.getElementById("clicked-letters").innerHTML = "clicked-letters: " + lettersClicked
+        ///// Validate that the user is not clicking the same letter;
+        attempts--;
+        document.getElementById("guesses-left").innerHTML = "guesses-left: " + attempts;
+        lettersClicked.push(letterInput);
+        document.getElementById("clicked-letters").innerHTML = "clicked-letters: " + lettersClicked;
 
         if (letterInput === computerLetter) {
             wins++
             document.getElementById("Wins").innerHTML = "Wins: " + wins
-            document.getElementById("info").innerHTML = "you won, let's start again"
+            document.getElementById("info").innerHTML = "Congratulations! You won!"
 
         }
         else {
@@ -74,6 +79,6 @@ function clean() {
     game()
 }
 
-game()
+game();
 
 
